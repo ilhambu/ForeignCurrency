bedf154fd3cbce27d0083b143aff87c4c8a1004e
ACS-Ilham <ACS-Ilham@ACS-Ilham>
2018-10-12 19:27:08 +0700

    First Commit

---
+++ src/main/java/com/acs/main/Application.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acs.main;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+/**
+ *
+ * @author ACS-Ilham
+ */
+@SpringBootApplication
+@EnableJpaRepositories
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SpringApplication.run(Application.class, args);
+        
+    }
+    
+}
